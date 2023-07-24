3a40187f1a698e91ea60d369c1289b8ad8de8fce
Keerti Koya <koyakeerti@gmail.com>
2023-07-24 10:44:55 -0700

    cleaning data + adding comments

--- Keerti/scripts/cleaning_data.R
+++ Keerti/scripts/cleaning_data.R
@@ -2,9 +2,6 @@ library(tidymodels)
 library(dplyr)
 library(reshape2)
 library(rsample)
-library(Metrics)
-library(parsnip)
-library(yardstick)
 library(ggplot2)
 library(tidyverse)
 library(imputeTS)
@@ -19,77 +16,13 @@ data$toohighblsugar <- as.numeric(gsub("\\00:00:0","",data$toohighblsugar))
 data$toohighblsugar[is.na(data$toohighblsugar)] <- mean(data$toohighblsugar,na.rm=TRUE)
 data$toohighblsugar <- round(data$toohighblsugar, digits = 1)
 
-# exploring data correlations
-# data_nutrition <- select(data, nutrition1, fruitveg20, avgsodaperday20, twoplussoda,
-#                    nsugardrinkperday20, avgsugarperday20, nsodasugarperday20,
-#                    avgsodasugarperday20, ssb, checkedbp20_q1, diabetes20, ageatdiabetes,
-#                    diabcntrlmeds, toohighblsugar)
-# 
-# data_other <- select(data, colonoscopy10yr_45, checkedbp20_q1, diabetes20, ageatdiabetes,
-#                      diabcntrlmeds, toohighblsugar, everasthma, currentasthma20,
-#                      firsttoldasthma, stillasthmaall)
-# 
-# data_smoking <- select(data, cpd20a, everydaycpda, heavysmoker20a,
-#                        smokecat,sourcelastcig, 
-#                        cost20cigarettes, cigpurchase20) # mb not
-# data_alcohol <- select(data, drinker, daysalc30, averagedrink20,
-#                        heavydrink20, bingenew)
-# 
-# data_suicide <- select(data, thinksuicide, triedsuicide, ipvphy, insultipv,
-#                        lowinchousing20, delaypayrent, rodentsstreet)
-# 
-# data_cancer <- select(data, evercolon20, colonoscopy10yr20, evercolon20_45,
-#                       colonoscopy10yr_45,
-#                       cpd20a, everydaycpda, heavysmoker20a,
-#                       smokecat,sourcelastcig, 
-#                       cost20cigarettes, cigpurchase20,
-#                       smokeecig12m20_q1, smokeecig30days20_q1,
-#                       smokehookah12m_q1)
-# 
-# data_immune <- select(data, fluvaccineshot, whereflu20, avgsodaperday20, 
-#                       insuredgateway20, insure20r, insured, insure5,
-#                       pcp20, medplace, didntgetcare20, regularrx,
-#                       skiprxcost)
-
-# data_alcohol <- select(data, agegroup, agegroup5, agegroup6, age21up, age25up, age40new,                    
-#                        age45up, age50up, age18_64, newrace, newrace6, imputed_neighpovgroup4_1519, 
-#                        imputed_povertygroup, imputed_povgroup3, imputed_pov200,
-#                        drinker, daysalc30, averagedrink20, heavydrink20, bingenew)
-# 
-# data_diabetes <- select(data, agegroup, agegroup5, agegroup6, age21up, age25up, age40new,                    
-#                         age45up, age50up, age18_64, bmi, weightall, weight20in4, weight20in5,
-#                         fruitveg20, avgsodaperday20, twoplussoda, nsugardrinkperday20, avgsugarperday20,
-#                         nsodasugarperday20, avgsodasugarperday20, ssb, exercise20,
-#                         cyclingfreq, cycling20, swim, imputed_neighpovgroup4_1519, 
-#                         imputed_povertygroup, imputed_povgroup3, imputed_pov200,
-#                         toldhighbp20, toldprescription20, takingmeds20, checkedbp20_q1, diabetes20,
-#                         ageatdiabetes, diabcntrlmeds, toohighblsugar)
-# 
-# data_smoker <- select(data, agegroup, agegroup5, agegroup6, age21up, age25up, age40new,                    
-#                       age45up, age50up, age18_64, bmi, weightall, weight20in4, weight20in5,
-#                       fruitveg20, avgsodaperday20, twoplussoda, nsugardrinkperday20, avgsugarperday20,
-#                       nsodasugarperday20, avgsodasugarperday20, ssb, exercise20,
-#                       cyclingfreq, cycling20, swim, imputed_neighpovgroup4_1519, 
-#                       imputed_povertygroup, imputed_povgroup3, imputed_pov200,
-#                       mood1, mood2, mood3, mood4, mood5, mood6, mood9, mood8, mood11)
-
-
+# age, poverty, insurance, access to healthcare, immunizations
 data_immunizations <- select(data, agegroup, agegroup5, agegroup6, age21up, age25up, age40new,
                              age45up, age50up, age18_64, imputed_neighpovgroup4_1519,
                              imputed_povertygroup, imputed_povgroup3, imputed_pov200,
                              generalhealth, insuredgateway20, insured, insure5, pcp20,
                              medplace, didntgetcare20, regularrx, skiprxcost,toldprescription20,
                              takingmeds20, fluvaccineshot, whereflu20)
-
-# data_insurance <- select(data, agegroup, agegroup5, agegroup6, age21up, age25up, age40new,                    
-#                          age45up, age50up, age18_64, imputed_neighpovgroup4_1519, 
-#                          imputed_povertygroup, imputed_povgroup3, imputed_pov200,
-#                          generalhealth, insuredgateway20, insured, insure5, pcp20, 
-#                          medplace, didntgetcare20, regularrx, skiprxcost,toldprescription20, takingmeds20,
-#                          difficultdailyact, assistdevice)
-
-
-names(data)
 
 # tile plot
 dataCors <- data_immunizations |>
