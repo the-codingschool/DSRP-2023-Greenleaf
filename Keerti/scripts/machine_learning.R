0039b9969baf064422770816dda1ba64a7fe9727
Keerti Koya <koyakeerti@gmail.com>
2023-07-28 13:05:57 -0700

    added knn model

--- Keerti/scripts/machine_learning.R
+++ Keerti/scripts/machine_learning.R
@@ -11,6 +11,9 @@ library(corrplot)
 library(parsnip)
 library(Metrics)
 library(caret)
+library(MLmetrics)
+library(class)
+
 
 # removing not significant variables
 final_data <- select(new_data, -didntgetcare20)
@@ -35,8 +38,8 @@ ggplot(pca_vals, aes(PC1, PC2, color = fluvaccineshot)) +
 
 str(final_data)
 
-# Train/Test Split ####
 
+# Train/Test Split ####
 final_data <- mutate(final_data,
                      fluvaccineshot = as.factor(fluvaccineshot))
 
@@ -75,6 +78,15 @@ forest_class_fit <- rand_forest() |>
 forest_class_fit$fit
 
 
+# K-Nearest Neighbours ####
+target_category <- train[,15]
+test_category <- test[,15]
+knn_model <- knn(train,test,cl=target_category,k=13)
+
+
+# Support Vector Machine ####
+
+
 # Error Evaluation ####
 
 results <- test
@@ -83,36 +95,30 @@ results$log_pred <- predict(log_fit,test)$.pred_class
 results$boost_pred <- predict(boost_class_fit, test)$.pred_class
 results$forest_pred <- predict(forest_class_fit, test)$.pred_class
 
-
-## Logistic Regression F1: 0.6721 ####
-results$actual <- factor(results$fluvaccineshot, levels = c("1","2"))
-results$pred_lp <- factor(results$log_pred, levels = c("1","2"))
-confusionMatrix(results$pred_lp, results$actual, mode = "everything", positive="1")
-
-
-## Boosted Trees F1: 0.6595 ####
-results$actual <- factor(results$fluvaccineshot, levels = c("1","2"))
-results$pred_bp <- factor(results$boost_pred, levels = c("1","2"))
-confusionMatrix(results$pred_bp, results$actual, mode = "everything", positive="1")
-
-
-## Random Forest F1: 0.6446 ####
-results$actual <- factor(results$fluvaccineshot, levels = c("1","2"))
-results$pred_fp <- factor(results$forest_pred, levels = c("1","2"))
-confusionMatrix(results$pred_fp, results$actual, mode = "everything", positive="1")
-
-# install.packages("MLmetrics")
-library("MLmetrics")
-
-
-F1_Score(as.numeric(results$actual), as.numeric(results$pred_lp))
-F1_Score(as.numeric(results$actual), as.numeric(results$pred_bp))
-F1_Score(as.numeric(results$actual), as.numeric(results$pred_fp))
-
 actual <- results$actual
 pred_lp <- results$pred_lp
 pred_bp <- results$pred_bp
 pred_fp <- results$pred_fp
+fluvaccineshot <- results$fluvaccineshot
+
+
+## Logistic Regression F1: 0.6721 ####
+actual <- factor(fluvaccineshot, levels = c("1","2"))
+pred_lp <- factor(results$log_pred, levels = c("1","2"))
+confusionMatrix(pred_lp, actual, mode = "everything", positive="1")
+
+
+## Boosted Trees F1: 0.6595 ####
+actual <- factor(fluvaccineshot, levels = c("1","2"))
+pred_bp <- factor(results$boost_pred, levels = c("1","2"))
+confusionMatrix(pred_bp, actual, mode = "everything", positive="1")
+
+
+## Random Forest F1: 0.6446 ####
+actual <- factor(fluvaccineshot, levels = c("1","2"))
+pred_fp <- factor(results$forest_pred, levels = c("1","2"))
+confusionMatrix(pred_fp, actual, mode = "everything", positive="1")
+
 
 errors <- data.frame(mae = c(mae(as.numeric(actual), as.numeric(pred_lp)),
                              mae(as.numeric(actual), as.numeric(pred_bp)), 
@@ -122,4 +128,19 @@ errors <- data.frame(mae = c(mae(as.numeric(actual), as.numeric(pred_lp)),
                               rmse(as.numeric(actual), as.numeric(pred_fp))),
                      modelType = c("logRegression", "boostTree", "randForest"))
 errors
+
+#      mae      rmse       modelType
+# 1 0.3607532 0.6006274 logRegression
+# 2 0.3785927 0.6152988     boostTree
+# 3 0.3885035 0.6233005    randForest
+
+
+## KNN Accuracy: 95.63925% ####
+conf_matrix <- table(knn_model,test_category)
+accuracy <- function(x){sum(diag(x)/(sum(rowSums(x)))) * 100}
+accuracy(conf_matrix)
+
+
+
+
 
